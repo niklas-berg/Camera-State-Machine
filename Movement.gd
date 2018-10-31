a68a4b938bc2593bbe86ed12570ebd96b8fd3bef
nicko <niklaas.berg@gmail.com>
2018-11-01 00:14:27 +0100

    Put both movement and camera transform computations in _process() -- Hopefully eliminating jitter

--- Movement.gd
+++ Movement.gd
@@ -1,15 +1,20 @@
 extends MeshInstance
 
-
 var camera: Basis
+var prevPos: Vector3
+var desiredPos: Vector3
+var preA: Vector3
+var postB: Vector3
+onready var target: Spatial = get_node("/root/Node/Target")
+
+func _ready():
+	desiredPos = target.global_transform.origin
+	preA = target.global_transform.origin
 
 func _process(delta):
 	camera = $"/root/Node/KinematicBody/Camera".global_transform.basis
-
-func _physics_process(delta):
 	
 	var dir = Vector3()
-	
 	if(Input.is_action_pressed("ui_up")):
 		dir += -camera[2]
 	if(Input.is_action_pressed("ui_down")):
@@ -22,7 +27,15 @@ func _physics_process(delta):
 		dir[1] += 0.1
 	if(Input.is_action_pressed("crouch")):
 		dir[1] += -0.1
+	dir = dir.normalized() * 1.0
 
-	dir = dir.normalized() * 0.25
+	var speed = 0.50
+	desiredPos += dir * 0.50 * speed
+	postB = desiredPos * 1.0 * speed
 
-	$"/root/Node/Target".global_transform.origin += dir
+	preA = target.global_transform.origin
+
+func _physics_process(delta):
+	target.global_transform.origin = target.global_transform.origin.cubic_interpolate(desiredPos, preA, postB, delta * 25.0)
+
+	
--- UnityBoi.gd
+++ UnityBoi.gd
@@ -10,80 +10,78 @@ var yaw: float
 var pitch: float
 var t: float
 var previousRotation: Quat
-#var previousPosition: Vector3
 var offset: Vector3
 var ray: RayCast
 var hit
 var colliding: bool
+
+var desiredCamPos
+var collisionPoint
+var newRotation
 
 
 func enter():
 	colliding = false
 	ray = RayCast.new()
 	target.add_child(ray)
-#	camera.add_child(ray)
-#	$"/root/Node/KinematicBody".add_child(ray)
 	ray.enabled = true
 	ray.set_collision_mask_bit(0, true)
 	ray.set_collision_mask_bit(1, false)
 	
 	previousRotation = Quat(camera.global_transform.basis)
-#	previousPosition = camera.global_transform.origin
 	offset = camera.global_transform.origin - target.global_transform.origin
+	
+	# This is uggu, but needed when placing quat calculations in _process().
+	# Maybe it's unecessary.
+	newRotation = Quat(Basis(Vector3(deg2rad(pitch), deg2rad(yaw), 0)))
+	desiredCamPos = target.global_transform.origin + newRotation * offset
+	collisionPoint = check_collision(desiredCamPos)
 
 func exit():
 	pass
 
 func _process(_delta):
 	mouseDelta = Vector2(0,0)
-
-func update(_delta):
-	yaw -= mouseDelta.x * mouseSens
-	pitch += mouseDelta.y * mouseSens
-	pitch = clamp(pitch, -30, 30)
-
-	var newRotation = Quat(Basis(Vector3(deg2rad(pitch), deg2rad(yaw), 0)))
-	if !colliding:
-		newRotation = previousRotation.slerp(newRotation, _delta * damp)
+	# |> A
+	newRotation = Quat(Basis(Vector3(deg2rad(pitch), deg2rad(yaw), 0)))
+	newRotation = previousRotation.slerp(newRotation, _delta * damp)
 	previousRotation = newRotation
-	
-	var desired_cam_pos = target.global_transform.origin + newRotation * offset
+	desiredCamPos = target.global_transform.origin + newRotation * offset
+	collisionPoint = check_collision(desiredCamPos)
+	# |) A
+func update(_delta):
 
-#	camera.global_transform.origin = target.global_transform.origin + newRotation * offset
-	var collision: Vector3 = check_collision(desired_cam_pos)
-	print(colliding)
-	if(collision):
+	# |< A
+
+	if(collisionPoint):
 		colliding = true
-		camera.global_transform.origin = collision
-#		camera.global_transform.origin = camera.global_transform.origin.slerp(collision, _delta * 25)
-#		camera.global_transform.origin = previousPosition.linear_interpolate(collision, _delta * 10)
+		camera.global_transform.origin = collisionPoint
+#		camera.global_transform.origin = camera.global_transform.origin.linear_interpolate(collisionPoint, _delta * 25)
 	else:
 		colliding = false
-		camera.global_transform.origin = desired_cam_pos
+		camera.global_transform.origin = desiredCamPos
 #		camera.global_transform.origin = camera.global_transform.origin.linear_interpolate(desired_cam_pos, _delta * 5)
-#	previousPosition = camera.global_transform.origin
 
 	camera.look_at(target.global_transform.origin, Vector3(0, 1, 0))
-	
 
-#	check_collision(Vector3(0,0,0))
 	debug()
 
 func handle_input(_event):
 	if _event is InputEventMouseMotion:
 		mouseDelta = _event.relative
-#		print(mouseDelta)
+	yaw -= mouseDelta.x * mouseSens
+	pitch += mouseDelta.y * mouseSens
+	pitch = clamp(pitch, -30, 30)
 
 func check_collision(desired_cam_pos: Vector3):
 	
 	ray.force_raycast_update()
-#	ray.cast_to = -(target.global_transform.origin - camera.global_transform.origin)
 	ray.cast_to = -(target.global_transform.origin - desired_cam_pos)
 	hit = ray.get_collision_point()
 	write_label("Key3", "ray collision:")
 	var collider = ray.get_collider()
 	write_label("Value3", str(collider))
-#	print(collider)
+
 	if collider:
 		return hit
 	else:
